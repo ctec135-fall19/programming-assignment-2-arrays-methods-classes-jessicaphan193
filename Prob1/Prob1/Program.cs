5293bdd081f283a7f61e596bc7ee1244d155e474
j <j.phan2@students.clark.edu>
2019-10-14 04:32:28 -0700

    Prob 2 and Prob 3, not done

---
+++ Prob1/Prob1/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prob1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Region 1
+            Console.WriteLine("REGION 1");
+
+            string[] strArray = new string[] { "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten" };
+
+            foreach (string element in strArray)
+            {
+                Console.WriteLine(element);
+            }
+            #endregion
+
+            #region Region 2
+            Console.WriteLine("REGION 2");
+
+            int[] intArray = new int[10];
+
+            for (int i = 0; i < intArray.Length; i++)
+            {
+                Console.WriteLine(intArray[i] = i * 10);
+            }
+            #endregion
+
+            #region Region 3
+            Console.WriteLine("REGION 3");
+
+            for (int i = 1; i <= 12; i++)
+            {
+                for (int j = 1; j <= 12; j++)
+                {
+                    Console.Write((i * j).ToString() + "\t");
+                }
+                Console.WriteLine();
+            }
+            #endregion
+        }
+    }
+}
---
+++ Prob1/Prob2/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prob2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Region 1
+            Console.WriteLine("VALUE");
+
+            int myInt = 21;
+            Console.WriteLine("Before:\t{0}\n," myInt);
+
+            callByValue(myInt);
+            Console.WriteLine("After:\t{0}\n," myInt);
+            #endregion
+
+
+            #region Region 2
+            Console.WriteLine("REFERENCE");
+
+            int[] myIntRef = new int[] { 10, 20, 30, 40, 50 };
+            Console.WriteLine("Before:");
+
+            for (int i = 0; i < myIntRef.Length; i++)
+            {
+                Console.WriteLine("{0}", myIntRef[i]);
+            }
+
+            Console.WriteLine("After:");
+            callByReference(myIntRef);
+
+            for (int i = 0; i < myIntRef.Length; i++)
+            {
+                Console.WriteLine("{0}", myIntRef[i]);
+            }
+            #endregion
+
+            //Not finished
+            #region Region 3
+            Console.WriteLine("OVERLOADING");
+
+            Console.WriteLine("");
+            #endregion
+
+
+
+
+
+        }
+
+    }
+}
